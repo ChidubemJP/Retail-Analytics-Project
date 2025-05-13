f0ccd75d034d2b96266b4378a68eb2a8c9f89fd5
ChidubemJP <chidubem.nwabunze@gmail.com>
2025-05-12 20:12:41 -0400

    total revenue last 12 months

--- sql/q01_total_revenue_last_12_months.sql
+++ sql/q01_total_revenue_last_12_months.sql
@@ -1,1 +1,6 @@
+-- Q1: Total revenue generated in the last 12 months
+SELECT 
+  ROUND(SUM(total_amount), 2) AS Total_Revenue
+FROM `Order`
+WHERE order_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 12 MONTH);
 
