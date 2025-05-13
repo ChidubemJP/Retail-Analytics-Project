555acf95081769cad3be464584504e07d5c55539
ChidubemJP <chidubem.nwabunze@gmail.com>
2025-05-12 20:15:27 -0400

    Q02 - top 10 products by units sold

--- sql/q02_top_10_products_by_units.sql
+++ sql/q02_top_10_products_by_units.sql
@@ -1,1 +1,11 @@
+-- Q2: Top 10 products by total units sold
+SELECT 
+    p.product_id, 
+    p.product_name, 
+    SUM(oi.quantity) AS Units_Sold
+FROM Product p
+JOIN Order_Item oi ON p.product_id = oi.product_id
+GROUP BY p.product_id, p.product_name
+ORDER BY Units_Sold DESC
+LIMIT 10;
 
