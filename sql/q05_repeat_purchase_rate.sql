23d8411e0ba1289cf3981706d65235414443f8d0
ChidubemJP <chidubem.nwabunze@gmail.com>
2025-05-12 20:23:57 -0400

    Q05 - repeat purchase rate

---
+++ sql/q05_repeat_purchase_rate.sql
@@ -0,0 +1,8 @@
+-- Q5: Repeat purchase rate (customers with more than one order)
+SELECT 
+    customer_id, 
+    COUNT(order_id) AS No_Of_Orders
+FROM `Order`
+GROUP BY customer_id
+HAVING No_Of_Orders > 1
+ORDER BY No_Of_Orders DESC;
